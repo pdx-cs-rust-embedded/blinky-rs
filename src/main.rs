538fc4a6415ec73e276199a5854bf97fd785d1db
Bart Massey <bart.massey@gmail.com>
2023-07-11 16:38:57 -0700

    switched to driving offboard LED on P8 (0.10)

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,12 @@ fn init() -> ! {
     let p = pac::Peripherals::take().unwrap();
     let p0parts = p0::Parts::new(p.P0);
 
-    let _col1 = p0parts.p0_28.into_push_pull_output(Level::Low);
-    let mut row1 = p0parts.p0_21.into_push_pull_output(Level::Low);
+    let mut led = p0parts.p0_10.into_push_pull_output(Level::Low);
 
     loop {
-        row1.set_high().unwrap();
+        led.set_high().unwrap();
         delay();
-        row1.set_low().unwrap();
+        led.set_low().unwrap();
         delay();
     }
 }

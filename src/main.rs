f2fbc8caa82c3bc2657c8035489d63922eb8c5d6
Bart Massey <bart.massey@gmail.com>
2023-07-13 00:42:28 -0700

    cleaned up a bunch of debugging code

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,13 @@ use microbit::{
         timer::Timer,
     },
 };
-use panic_halt as _;
+use rtt_target::{rtt_init_print, rprintln};                                   
+use panic_rtt_target as _;                                                    
+
 
 #[entry]
 fn init() -> ! {
+    rtt_init_print!();
     let mut board = Board::take().unwrap();
     let mut timer = Timer::new(board.TIMER0);
 
@@ -20,8 +23,10 @@ fn init() -> ! {
 
     loop {
         board.display_pins.row1.set_high().unwrap();
+        rprintln!("high");
         timer.delay_ms(500u16);
         board.display_pins.row1.set_low().unwrap();
+        rprintln!("low");
         timer.delay_ms(500u16);
     }
 }
